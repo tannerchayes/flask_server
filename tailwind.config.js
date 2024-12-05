8380efca159196fbb985cd404144062317269a90
Tanner <tanner@dhayes.net>
2024-12-05 15:53:40 -0500

    the extras

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      // "./src/**/*.{html,js}",
+    '**/templates/**/*.html',
+    '**/static/**/*.js',
+    '**/*.py'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
